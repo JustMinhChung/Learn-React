dbfb1c98e58e64002b252bd220f37fdb38f8cc6c
JustMinhChung <vominhchung01937@gmail.com>
2026-05-24 22:26:48 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <p>Website đầu tiên của tôi</p>
+
+      <button>Click me</button>
+    </div>
+  )
+}
+
+export default App
